695c22dd9fa7de140fa0002b222f9ef9b05f79a4
AlienAtSystem <gerald.malsch@web.de>
2025-12-11 08:43:39 +0100

    [FIX] pointcrawl-tool: Set compute field to recursive

--- pointcrawl_tool/models/area.py
+++ pointcrawl_tool/models/area.py
@@ -17,7 +17,7 @@ class Area(models.Model):
     location_ids = fields.One2many('point.location', 'area_id', 'Locations')
 
     color = fields.Char('Color')
-    display_color = fields.Char('Display Color', compute='_compute_display_color')
+    display_color = fields.Char('Display Color', compute='_compute_display_color', recursive=True)
 
     @api.constrains('parent_id')
     def _check_parent_id(self):
